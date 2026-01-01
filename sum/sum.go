e214c461d8b725aab989be736a9f40d28d780595
7tomo <eeegor2003@mail.ru>
2026-01-01 13:59:39 +0800

    сумма чисел

--- sum/sum.go
+++ sum/sum.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-sum(2,10)
+	sum(2, 10)
+	sum(8, 8)
+	sum(10, 10)
+	sum(11, 13)
 }
 
-func sum(a int, b int){
+func sum(a int, b int) {
 	var c = a + b
 	fmt.Println("Сумма чисел: ", c)
 }
